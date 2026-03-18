e3101662939d721a231530b941f6307b6042acd3
sumit nagar <sumitnagarkvv.008125@gmail.com>
2026-03-18 10:04:33 +0530

    bypass login in protected route

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,5 +1,3 @@
-import { Navigate } from 'react-router-dom';
-import { useAuth } from '../contexts/AuthContext';
 import { ReactNode } from 'react';
 
 interface ProtectedRouteProps {
@@ -7,16 +5,6 @@ interface ProtectedRouteProps {
 }
 
 const ProtectedRoute: React.FC<ProtectedRouteProps> = ({ children }) => {
-	const { isAuthenticated, isInitializing } = useAuth();
-
-	if (isInitializing) {
-		return null;
-	}
-
-	if (!isAuthenticated) {
-		return <Navigate to="/login" replace />;
-	}
-
 	return <>{children}</>;
 };
 
